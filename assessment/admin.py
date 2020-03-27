d5cc42aa84e2ef36538814c423bfd8da80820c28
Techoplite <oriccimirko@gmail.com>
2020-03-27 16:02:39 +0000

    Add side divisions

--- assessment/admin.py
+++ assessment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Assessment, Problem, Answer
 
-# Register your models here.
+admin.site.register(Assessment)
+admin.site.register(Problem)
+admin.site.register(Answer)
