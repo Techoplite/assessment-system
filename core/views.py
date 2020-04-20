18af58524b05482b22ab0aa8b8726f74c2852dbd
Techoplite <oriccimirko@gmail.com>
2020-04-20 16:52:08 +0100

    Fix student results not showing in student home.

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from assessment.models import Assessment
 from accounts.models import User
+from assessment.models import Result
 
 
 def home(request):
     template_name = 'home.html'
+    student_results = None
 
     # Verify that the user is authenticated
     # and fetches the related assessments.
@@ -13,10 +15,14 @@ def home(request):
         user_id = request.user.id
         current_user = User.objects.get(id=user_id)
         user_assessments = Assessment.objects.filter(creator=current_user).exclude(title='')
+        # Fetch student results.
+        student_results = Result.objects.filter(student=current_user)
+
     else:
         user_assessments = None
     context = {
         'user_assessments': user_assessments,
+        'student_results': student_results,
     }
     return render(request, template_name, context)
 
